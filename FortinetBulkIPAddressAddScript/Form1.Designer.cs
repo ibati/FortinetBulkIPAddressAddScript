15f7e364cbb388e23ea9c487e2de0b8bda3c3285
ibrahim batı <ibrahimbati@gmail.com>
2021-11-04 12:40:53 +0300

    First upload

---
+++ FortinetBulkIPAddressAddScript/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace FortinetBulkIPAddressAddScript
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richIPs = new System.Windows.Forms.RichTextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lbDescrition2 = new System.Windows.Forms.Label();
+            this.lbDescrition1 = new System.Windows.Forms.Label();
+            this.picGithub = new System.Windows.Forms.PictureBox();
+            this.picTwitter = new System.Windows.Forms.PictureBox();
+            this.picWebsite = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picGithub)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTwitter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWebsite)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richIPs
+            // 
+            this.richIPs.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richIPs.Location = new System.Drawing.Point(55, 60);
+            this.richIPs.Name = "richIPs";
+            this.richIPs.Size = new System.Drawing.Size(274, 497);
+            this.richIPs.TabIndex = 0;
+            this.richIPs.Text = "";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCreate.Location = new System.Drawing.Point(380, 60);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(150, 60);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create Script";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnClear.ForeColor = System.Drawing.Color.Maroon;
+            this.btnClear.Location = new System.Drawing.Point(380, 140);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(150, 60);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lbDescrition2
+            // 
+            this.lbDescrition2.Font = new System.Drawing.Font("Consolas", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbDescrition2.Location = new System.Drawing.Point(377, 363);
+            this.lbDescrition2.Name = "lbDescrition2";
+            this.lbDescrition2.Size = new System.Drawing.Size(150, 84);
+            this.lbDescrition2.TabIndex = 4;
+            this.lbDescrition2.Text = "Additionally, added IP addresses are included in the deny-ip-grp group.";
+            // 
+            // lbDescrition1
+            // 
+            this.lbDescrition1.Font = new System.Drawing.Font("Consolas", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbDescrition1.Location = new System.Drawing.Point(380, 223);
+            this.lbDescrition1.Name = "lbDescrition1";
+            this.lbDescrition1.Size = new System.Drawing.Size(150, 127);
+            this.lbDescrition1.TabIndex = 5;
+            this.lbDescrition1.Text = "With this application, the IP addresses you paste in lines are converted into a b" +
+    "ulk IP address addition script for Fortinet.";
+            // 
+            // picGithub
+            // 
+            this.picGithub.Image = ((System.Drawing.Image)(resources.GetObject("picGithub.Image")));
+            this.picGithub.Location = new System.Drawing.Point(320, 607);
+            this.picGithub.MaximumSize = new System.Drawing.Size(24, 24);
+            this.picGithub.MinimumSize = new System.Drawing.Size(24, 24);
+            this.picGithub.Name = "picGithub";
+            this.picGithub.Size = new System.Drawing.Size(24, 24);
+            this.picGithub.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGithub.TabIndex = 6;
+            this.picGithub.TabStop = false;
+            this.picGithub.Click += new System.EventHandler(this.picGithub_Click);
+            // 
+            // picTwitter
+            // 
+            this.picTwitter.Image = ((System.Drawing.Image)(resources.GetObject("picTwitter.Image")));
+            this.picTwitter.Location = new System.Drawing.Point(350, 607);
+            this.picTwitter.MaximumSize = new System.Drawing.Size(24, 24);
+            this.picTwitter.MinimumSize = new System.Drawing.Size(24, 24);
+            this.picTwitter.Name = "picTwitter";
+            this.picTwitter.Size = new System.Drawing.Size(24, 24);
+            this.picTwitter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picTwitter.TabIndex = 7;
+            this.picTwitter.TabStop = false;
+            this.picTwitter.Click += new System.EventHandler(this.picTwitter_Click);
+            // 
+            // picWebsite
+            // 
+            this.picWebsite.Image = ((System.Drawing.Image)(resources.GetObject("picWebsite.Image")));
+            this.picWebsite.Location = new System.Drawing.Point(290, 607);
+            this.picWebsite.MaximumSize = new System.Drawing.Size(16, 16);
+            this.picWebsite.MinimumSize = new System.Drawing.Size(24, 24);
+            this.picWebsite.Name = "picWebsite";
+            this.picWebsite.Size = new System.Drawing.Size(24, 24);
+            this.picWebsite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWebsite.TabIndex = 8;
+            this.picWebsite.TabStop = false;
+            this.picWebsite.Click += new System.EventHandler(this.picWebsite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 651);
+            this.Controls.Add(this.picWebsite);
+            this.Controls.Add(this.picTwitter);
+            this.Controls.Add(this.picGithub);
+            this.Controls.Add(this.lbDescrition1);
+            this.Controls.Add(this.lbDescrition2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.richIPs);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Fortinet Bulk IP Address Add Script";
+            ((System.ComponentModel.ISupportInitialize)(this.picGithub)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTwitter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWebsite)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richIPs;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lbDescrition2;
+        private System.Windows.Forms.Label lbDescrition1;
+        private System.Windows.Forms.PictureBox picGithub;
+        private System.Windows.Forms.PictureBox picTwitter;
+        private System.Windows.Forms.PictureBox picWebsite;
+    }
+}
+
---
+++ FortinetBulkIPAddressAddScript/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FortinetBulkIPAddressAddScript
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string conf1 = "# config firewall address";
+            string code1 = "    edit ";
+            string code2 = Environment.NewLine + "        set subnet ";
+            string code3 = "/32";
+            string code4 = Environment.NewLine + "    next";
+            string conf2 = "end";
+
+            string conf3 = "# config firewall addrgrp";
+            string code5 = Environment.NewLine + "    edit deny-ips";
+            string code6 = Environment.NewLine + "        set member ";
+            string code7 = Environment.NewLine + "    next";
+            string conf4 = "end";
+
+            var ipadress = new StringBuilder();
+            foreach (var line in richIPs.Lines)
+            {
+                ipadress.AppendLine(code1 + line + code2 + line + code3 + code4);
+            }
+
+            var ipgroup = new StringBuilder();
+            foreach (var line in richIPs.Lines)
+            {
+                ipgroup.Append(line + " ");
+            }
+
+            File.WriteAllText(@"FortinetBulkIPAddressScript.txt", conf1 + Environment.NewLine + ipadress.ToString() + conf2 + Environment.NewLine + conf3 + code5 + code6 + ipgroup.ToString() + code7 + Environment.NewLine + conf4);
+        }
+
+        private void picGithub_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/ibati");
+        }
+
+        private void picWebsite_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://ibrahimbati.com");
+        }
+
+        private void picTwitter_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://twitter.com/ibati");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            richIPs.Text = String.Empty;
+        }
+    }
+}
